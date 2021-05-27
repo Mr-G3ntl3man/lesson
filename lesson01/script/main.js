9a9067d7bdb22d202315411ce24df24c57de2bd1
Mr. Junior <archi3run@gmail.com>
2021-05-27 22:53:26 +0300

    lesson01

---
+++ lesson01/script/main.js
@@ -0,0 +1,10 @@
+const money = null,
+	income = null,
+	addExpenses = null,
+	deposit = null,
+	mission = null,
+	period = null;
+
+alert('сообщение с любым текстом');
+
+console.log('сообщение с любым текстом');
