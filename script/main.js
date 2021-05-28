296724bec6baccf445df3fb0307ed2060d6a377a
Mr. Junior <archi3run@gmail.com>
2021-05-28 20:25:19 +0300

    lesson_2

---
+++ script/main.js
@@ -0,0 +1,21 @@
+const money = 0,
+	income = 'фриланс',
+	addExpenses = 'интернет, такси, коммуналка',
+	deposit = false,
+	mission = 0,
+	period = 1;
+
+console.log(typeof money);
+
+console.log(typeof income);
+
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(period === mission);
+
+console.log(addExpenses.toLowerCase().split(''));
+
+const budgetDay = 0;
+console.log(budgetDay);
