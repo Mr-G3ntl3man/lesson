91d8110717859099df2af5a2a34f4fbb3aa6ef2e
Mr. Junior <archi3run@gmail.com>
2021-06-01 18:16:38 +0300

    feat(main.js): create new function

--- script/main.js
+++ script/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let showString = (arg) => {
+	if (typeof arg === 'string') {
+		const a = arg.trim();
+		(a.length > 30) ? console.log(`${a.substring(0, 30)}...`) : console.log(a);
+	} else {
+		console.log('Введите строку!');
+	}
+};
+
+showString('       aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa                ');
