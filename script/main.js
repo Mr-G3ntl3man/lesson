23de8c4ac0ed503f6a210eb80a35314e56a2ac07
Mr. Junior <archi3run@gmail.com>
2021-06-03 23:52:08 +0300

    feat(main.js): create bad function

--- script/main.js
+++ script/main.js
@@ -4,29 +4,31 @@ const isNum = (n) => {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-const bot = (num) => {
-	return () => {
-		let start = prompt('Угадай число от 1 до 100', 50);
-		if (start === null) {
-			alert('Игра окончена');
-		} else if (isNum(start)) {
-			if (+start === num) {
-				alert('Поздравляю, Вы угадали!!!');
-			} else if (+start > num) {
-				alert('Загаданное число меньше');
-				return foo();
-			} else if (+start < num) {
-				alert('Загаданное число больше');
-				return foo();
-			}
-		} else {
-			alert('Введи число!');
-			return foo();
-		}
+const bot = (num, attempts = 10) => {
+	let start = prompt('Угадай число от 1 до 100', 50);
+
+	if (attempts === 0) {
+		alert('Попытки закончились');
 	}
 
+	if (start === null) {
+		alert('Игра окончена');
+		return;
+	} else if (isNum(start)) {
+		if (+start === num) {
+			alert('Поздравляю, Вы угадали!!!');
+			return;
+		} else if (+start > num) {
+			alert(`Загаданное число меньше, осталось попыток ${attempts}`);
+		} else if (+start < num) {
+			alert(`Загаданное число больше, осталось попыток ${attempts}`);
+		}
+	} else {
+		alert('Введи число!');
+	}
+
+	return bot(num, attempts--);
 };
 
-const foo = bot(42);
 
-foo();
+bot(42);
