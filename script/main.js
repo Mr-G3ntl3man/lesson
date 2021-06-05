338607a59f36c551ef3fe9fef95ab7baa8149eb0
Noob <arthur.pashchuk@yandex.ru>
2021-06-05 20:46:59 +0300

    fix(main.js): improved the code

--- script/main.js
+++ script/main.js
@@ -1,55 +1,28 @@
 'use strict';
 
-let lang = prompt('Введите язык "ru" или "en"', 'en');
+const lang = prompt('Введите язык "ru" или "en"', 'en'),
+	namePerson = prompt('Введите имя пользователя!', 'Артем'),
+	daysOfTheWeek = [
+		['ru', 'Дни недели на русском языке: понедельник, вторник, среда, четверг, пятница, суббота, воскресенье.'],
+		['en', 'Дни недели на английском языке: monday, tuesday, wednesday, thursday, friday, saturday, sunday.'],
+		[, 'Что-то пошло не так!'],],
+	map = new Map(daysOfTheWeek),
+	ru = daysOfTheWeek[0].toString().slice(3),
+	en = daysOfTheWeek[1].toString().slice(3);
 
-if (lang === 'en') {
-	console.log(`
-		Дни недели на английском языке: monday, tuesday, wednesday,
-		thursday, friday, saturday, sunday.
-	`);
-} else if (lang === 'ru') {
-	console.log(`
-		Дни недели на русском языке: понедельник, вторник, среда,
-		четверг, пятница, суббота, воскресенье.
-	`);
-} else {
-	console.log('Что-то пошло не так!');
-}
+
+console.log(map.get(lang) || map.get(undefined));
+
+if (lang === 'ru') console.log(ru);
+else if (lang === 'en') console.log(en);
+else console.log('Что-то пошло не так!');
 
 switch (lang) {
-	case 'en':
-		console.log(`
-		Дни недели на английском языке: monday, tuesday, wednesday,
-		thursday, friday, saturday, sunday.
-	`);
-		break;
-
-	case 'ru':
-		console.log(`
-		Дни недели на русском языке: понедельник, вторник, среда,
-		четверг, пятница, суббота, воскресенье.
-	`);
-		break;
-
-	default:
-		console.log('Что-то пошло не так!');
+	case 'ru': console.log(ru); break;
+	case 'en': console.log(en); break;
+	default: console.log('Что-то пошло не так!');
 }
 
-const daysOfTheWeek = new Map([
-	['ru', `Дни недели на русском языке: понедельник, вторник, среда,
-	четверг, пятница, суббота, воскресенье.`],
-
-	['en', `Дни недели на английском языке: monday, tuesday, wednesday,
-	thursday, friday, saturday, sunday.`],
-
-	[, 'Что-то пошло не так!'],
-
-]);
-
-console.log(daysOfTheWeek.has(lang) ? daysOfTheWeek.get(lang) : daysOfTheWeek.get(undefined))
-
-
-let namePerson = prompt('Введите имя пользователя!', 'Артем');
 
 (namePerson === 'Артем') ? console.log('Директор') :
 	(namePerson === 'Максим') ? console.log('Преподаватель') :
