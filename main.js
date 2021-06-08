b5406d34a9d7ca4145d82f706e34b340533ce307
Mr. Junior <archi3run@gmail.com>
2021-06-08 22:27:51 +0300

    -

---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const book = document.querySelectorAll('.book'),
+	spam = document.querySelector('.adv'),
+	h2 = document.querySelectorAll('.book h2 a'),
+	liBook = document.querySelectorAll('ul li');
+
+book[0].before(book[1]);
+book[0].after(book[4]);
+book[4].after(book[3]);
+book[3].after(book[5]);
+
+document.body.style.background = 'url(image/you-dont-know-js.jpg)';
+
+h2[4].innerHTML = 'Книга 3. this и Прототипы Объектов';
+
+spam.remove();
+
+liBook[3].after(liBook[2]);
+liBook[3].after(liBook[6]);
+liBook[6].after(liBook[8]);
+liBook[8].after(liBook[4]);
+liBook[4].after(liBook[5]);
+liBook[9].after(liBook[2]);
+
+liBook[25].innerHTML = '<li>Глава 8: За пределами ES6</li>';
+
+
+
+
+
+
+
+
+
+
