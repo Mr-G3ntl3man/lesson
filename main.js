71914f61cc12daa9b249592343fadf472e6179e2
Mr. Junior <archi3run@gmail.com>
2021-06-09 18:25:37 +0300

    fix(main.js): corrected code

--- main.js
+++ main.js
@@ -22,5 +22,11 @@ liBook[6].after(liBook[8]);
 liBook[8].after(liBook[4]);
 liBook[4].after(liBook[5]);
 liBook[9].after(liBook[2]);
+liBook[47].after(liBook[55]);
+liBook[55].after(liBook[49]);
+liBook[49].after(liBook[50]);
+liBook[48].after(liBook[52]);
+liBook[52].after(liBook[53]);
 
+console.log(liBook);
 liBook[25].innerHTML = '<li>Глава 8: За пределами ES6</li>';
