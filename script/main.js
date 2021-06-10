c6954b087c9cc411667ae41d7d1b28e9712122b8
Mr. Junior <archi3run@gmail.com>
2021-06-10 20:50:16 +0300

    -

--- script/main.js
+++ script/main.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const anyCombo = '0123456789ABCDEF',
+	btn = document.querySelector('.btn'),
+	htmlHexCode = document.querySelector('.hex-code');
+
+let hexCode;
+
+const generateCode = () => {
+	hexCode = '#';
+	for (var i = 0; i < 6; i++) {
+		hexCode += anyCombo[(Math.floor(Math.random() * 16))];
+	}
+}
+
+btn.addEventListener('click', () => {
+	generateCode();
+	document.body.style.backgroundColor = hexCode;
+	htmlHexCode.innerHTML = hexCode;
+	btn.style.color = hexCode;
+
+	generateCode()
+	htmlHexCode.style.color = hexCode;
+	btn.style.backgroundColor = hexCode;
+})
