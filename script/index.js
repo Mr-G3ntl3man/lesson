f4e0609dbee4943254847d421e2a5a323c7cdabf
Person <arthur.pashchuk@yandex.ru>
2021-06-12 21:54:10 +0300

    -

---
+++ script/index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const todoControl = document.querySelector('.todo-control'),
+	headerInput = document.querySelector('.header-input'),
+	todoList = document.querySelector('.todo-list'),
+	todoCompleted = document.querySelector('.todo-completed');
+
+
+const data = [
+	{
+		value: 'Дело первое',
+		completed: false,
+	},
+	{
+		value: 'Дело второе',
+		completed: true,
+	}
+];
+
+const render = function () {
+
+}
+
+
+
