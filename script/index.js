63c205d6dda4a7e339ba874c2adf5037760050e4
Person <arthur.pashchuk@yandex.ru>
2021-06-13 01:47:15 +0300

    -

--- script/index.js
+++ script/index.js
@@ -6,20 +6,61 @@ const todoControl = document.querySelector('.todo-control'),
 	todoCompleted = document.querySelector('.todo-completed');
 
 
-const data = [
-	{
-		value: 'Дело первое',
-		completed: false,
-	},
-	{
-		value: 'Дело второе',
-		completed: true,
-	}
-];
+const data = [];
 
-const render = function () {
+const render = () => {
+	todoList.textContent = ''
+	todoCompleted.textContent = ''
 
+	data.forEach((el) => {
+		const li = document.createElement('li')
+		li.classList.add('todo-item')
+
+		li.innerHTML = `
+			<span class="text-todo">${el.value}</span>
+			<div class="todo-buttons">
+				<button class="todo-remove"></button>
+				<button class="todo-complete"></button>
+			</div>`;
+
+		(el.completed) ? todoCompleted.prepend(li) : todoList.prepend(li);
+
+
+
+		const done = li.querySelector('.todo-complete'),
+			remove = li.querySelector('.todo-remove');
+
+		done.addEventListener('click', () => {
+			el.completed = !el.completed
+			render()
+		})
+
+		remove.addEventListener('click', () => {
+			li.remove();
+			data.forEach((prop, index) => { if (prop === el) { data.splice(index, 1) } })
+		})
+
+
+	})
 }
 
 
 
+todoControl.addEventListener('submit', (event) => {
+	event.preventDefault()
+
+	if (headerInput.value.trim() === '') {
+		headerInput.style.border = '1px solid red'
+	} else {
+		headerInput.style.border = 'none'
+		const newToDo = {
+			value: headerInput.value,
+			completed: false,
+		}
+		data.push(newToDo)
+		headerInput.value = ''
+		render()
+	}
+})
+
+render()
