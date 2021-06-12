576894a247147358dd7ee68795a50aa73f490407
Person <arthur.pashchuk@yandex.ru>
2021-06-12 20:36:11 +0300

    fix(main.js): corrected code

--- script/main.js
+++ script/main.js
@@ -1,63 +1,55 @@
 'use strict';
 
-const DomElement = function () {
-	this.bg;
-	this.width;
-	this.height;
-	this.fontSize;
-	this.selector;
-	this.text;
-	this.position;
-
-}
-
-DomElement.prototype.start = function () {
-	this.createElem()
-	this.addStyle()
-	this.addText()
-};
-
-DomElement.prototype.createElem = function () {
-	if (this.selector.startsWith('.')) {
-		return document.body.insertAdjacentHTML('beforeend', `<div class = '${this.selector.substring(1)}'></div>`)
+class DomElement {
+	constructor(
+		selector = '.new-block',
+		height = 500,
+		width = 500,
+		bg = '#049D03',
+		fontSize = 50,
+		text = 'Нужно больше правок!',
+		position = 'absolute',
+	) {
+		this.selector = selector;
+		this.width = width;
+		this.height = height;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.text = text;
+		this.position = position;
 	}
 
-	if (this.selector.startsWith('#')) {
-		return document.body.insertAdjacentHTML('beforeend', `<p id = '${this.selector.substring(1)}'></p>`)
+	createElem() {
+		const createHtml = (el) => {
+			const htmlEl = document.createElement(el);
+			(el === 'div') ? htmlEl.className = `${this.selector.substring(1)}` : htmlEl.id = `${this.selector.substring(1)}`
+			htmlEl.innerHTML = `${this.text}`
+			this.addStyle(htmlEl)
+			document.body.prepend(htmlEl)
+		}
+
+		if (this.selector.startsWith('.')) { createHtml('div') }
+		if (this.selector.startsWith('#')) { createHtml('p') }
 	}
-};
 
-DomElement.prototype.addStyle = function () {
-	const elem = document.querySelector(`${this.selector}`)
-	elem.style.cssText = `
-	background-color: ${this.bg};
-	width: ${this.width}px;
-	height: ${this.height}px;
-	font-size: ${this.fontSize}px;
-	position: ${this.position};
-	top: 0;
-	left: 0;
-	`
-};
-
-DomElement.prototype.addText = function () {
-	const elem = document.querySelector(`${this.selector}`)
-	elem.innerHTML = `${this.text}`
+	addStyle(el) {
+		const elem = document.querySelector(`${this.selector}`)
+		el.style.cssText = `
+		background-color: ${this.bg};
+		width: ${parseInt(this.width)}px;
+		height: ${parseInt(this.height)}px;
+		font-size: ${parseInt(this.fontSize)}px;
+		position: ${this.position};
+		top: 0;
+		left: 0;`
+	}
 }
 
-
-const newElem = new DomElement();
-newElem.selector = '#new-block';
-newElem.bg = '#049D03';
-newElem.width = 100;
-newElem.height = 100;
-newElem.fontSize = 18;
-newElem.position = 'absolute';
-newElem.text = 'Жду правки! ^_^'
+const newElem = new DomElement('#block', '200px', '400px', 'green', '30px');
 
 
 document.addEventListener('DOMContentLoaded', () => {
-	newElem.start()
+	newElem.createElem()
 
 	document.addEventListener('keydown', (event) => {
 		const elem = document.querySelector(`${newElem.selector}`)
