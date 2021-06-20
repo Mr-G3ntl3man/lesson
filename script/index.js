652df9fa7b13378f0bd361bc09a3470a07005f9f
Mr. Junior <archi3run@gmail.com>
2021-06-20 23:17:03 +0300

    -

--- script/index.js
+++ script/index.js
@@ -1,92 +1,66 @@
 'use strict'
 
-// const todoControl = document.querySelector('.todo-control'),
-// 	headerInput = document.querySelector('.header-input'),
-// 	todoList = document.querySelector('.todo-list'),
-// 	todoCompleted = document.querySelector('.todo-completed'),
-// 	btnSubmit = document.querySelector('.header-button');
-
 const userName = document.querySelector('.user__name'),
 	check = document.querySelector('.check'),
 	login = document.querySelector('.login'),
-	userList = document.querySelector('.user-list');
+	userList = document.querySelector('.user-list')
 
 
+const data = JSON.parse(localStorage.getItem('Person')) || []
 
+data.forEach(el => userList.insertAdjacentHTML('beforeend', `<li>Имя: ${el.name}, Фамилия: ${el.surname}, Дата регистрации: ${el.registrationDate} <button class='remove'>Удалить</button></li>`));
 
+const dateNow = () => {
+	const createZero = i => i < 10 ? `0${i}` : i
 
+	const date = new Date(),
+		today = date.toLocaleDateString('ru', { year: 'numeric', month: 'long', day: 'numeric', }),
+		hour = createZero(date.getHours()),
+		minute = createZero(date.getMinutes()),
+		second = createZero(date.getSeconds())
 
+	return `${today} - ${hour}:${minute}:${second}`
+}
 
+const collector = () => {
+	const name = prompt('Ведите имя и фамилию, через пробел!')
 
+	if (/^[а-яё]+ [а-яё]+$/i.test(name)) {
+		const prop = name.split(' '),
+			login = prompt('Ведите логин!'),
+			password = prompt('Ведите пароль!')
 
+		const newPerson = { name: prop[0], surname: prop[1], login: login, password: password, registrationDate: dateNow() }
+		data.push(newPerson)
+		userList.insertAdjacentHTML('beforeend', `<li>Имя: ${newPerson.name}, Фамилия: ${newPerson.surname}, Дата регистрации: ${newPerson.registrationDate} <button class='remove'>Удалить</button></li>`)
+		localStorage.setItem('Person', JSON.stringify(data))
+	} else {
+		alert('Ошибка')
+	}
+}
 
+const removePrerson = (el) => {
+	if (el.className === 'remove') {
+		data.forEach((prop, index) => { if (prop.registrationDate === el.parentNode.textContent.split(': ')[3].slice(0, -8)) data.splice(index, 1) })
 
-let data = [];
-if (localStorage.getItem('ToDo')) { data = JSON.parse(localStorage.getItem('ToDo')) }
+		el.parentNode.remove()
+		localStorage.setItem('Person', JSON.stringify(data))
+	}
+}
 
-const render = () => {
-	todoList.textContent = ''
-	todoCompleted.textContent = ''
-
-	data.forEach((el) => {
-		const li = document.createElement('li')
-		li.classList.add('todo-item')
-		li.innerHTML = `
-			<span class="text-todo">${el.value}</span>
-			<div class="todo-buttons">
-				<button class="todo-remove"></button>
-			</div>`;
-
-		(el.completed) ? todoCompleted.prepend(li) : todoList.prepend(li);
-
-
-		const done = li.querySelector('.todo-complete'),
-			remove = li.querySelector('.todo-remove');
-
-		done.addEventListener('click', () => {
-			li.style.animation = 'do 1s forwards'
-			el.completed = !el.completed
-
-			localStorage.setItem('ToDo', JSON.stringify(data))
-			render()
-		})
-
-		remove.addEventListener('click', () => {
-			li.classList.add('hidden')
-			setTimeout(() => { li.remove() }, 1000)
-
-			data.forEach((prop, index) => { if (prop === el) { data.splice(index, 1) } })
-			localStorage.setItem('ToDo', JSON.stringify(data))
-		})
+const logPerson = () => {
+	const login = prompt('Ведите логин!')
+	data.forEach(el => {
+		if (login === el.login) {
+			const password = prompt('Ведите пароль!');
+			(password === el.password) ? userName.innerHTML = el.name : alert('Не верный пароль!')
+		} else {
+			alert('Пользователь не найден!')
+		}
 	})
 }
 
-
-
-const collector = () => {
-	const newToDo = { value: headerInput.value, completed: false, }
-	data.push(newToDo)
-
-	localStorage.setItem('ToDo', JSON.stringify(data))
-	headerInput.value = ''
-	btnSubmit.classList.add('submit')
-	setTimeout(() => { btnSubmit.classList.remove('submit') }, 500)
-
-	render()
-}
-
-todoControl.addEventListener('submit', (event) => {
-	event.preventDefault();
-
-	if (headerInput.value.trim() === '') {
-		headerInput.classList.add('valid')
-		setTimeout(() => { headerInput.classList.remove('valid') }, 500)
-	} else collector()
-})
-
-render()
-
-
-
-
+check.addEventListener('click', collector)
+userList.addEventListener('click', el => removePrerson(el.target))
+login.addEventListener('click', logPerson)
 
