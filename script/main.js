5b64ae49e1d9ba5c99b5af844d2c9afbc0c21cf5
Mr. Junior <archi3run@gmail.com>
2021-06-30 00:23:25 +0300

    fix(main.js) corrected code

--- script/main.js
+++ script/main.js
@@ -355,10 +355,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 			const animCount = () => {
-				counter += 50
-				if (res > 10000) counter += 300 * 10
-				if (res > 100000) counter += 100000 * 10
-				if (res > 1000000) counter += 1000000 * 20
+				const kef = Math.ceil((res + 1) / 2000)
+				counter += 50 * kef
+
 				const animId = requestAnimationFrame(animCount);
 				(counter <= res) ? total.textContent = counter : cancelAnimationFrame(animId)
 			}
